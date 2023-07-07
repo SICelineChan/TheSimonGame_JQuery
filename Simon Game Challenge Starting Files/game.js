3749122b031707fd44b56b730709a8bc2e0e3fa0
Celine Chan <cctinlone@gmail.com>
2023-07-07 19:23:33 +0200

    1st commit

---
+++ Simon Game Challenge Starting Files/game.js
@@ -0,0 +1,8 @@
+let buttonColors = ["red", "blue","green", "yellow"]
+let randomChosenColor
+
+function nextSequence(){
+    let randomNum = Math.floor(Math.random()*4);
+    console.log(randomNum)
+}
+nextSequence();
