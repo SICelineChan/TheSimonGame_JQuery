5e15b88e9d4ed18da237b1a0e4325537f82c611b
Celine Chan <cctinlone@gmail.com>
2023-07-08 22:39:45 +0200

    explanation for myself added

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -5,20 +5,25 @@ let userClickedPattern = [];
 
 
 $(".btn").click(function() {
+    // when user click "this" which this button is, it will show the chosen color's id
+    // say if I clicked the red square, "this" will become the red square with the id red in the html
     let userChosenColor = $(this).attr("id")
     console.log(userChosenColor)
-    
+    //whenever the user clicked a square this will push the color into the empty array of userClickedPattern
     userClickedPattern.push(userChosenColor);
     console.log(userClickedPattern)
-
+    //the function to play the sound will play the corresponding sound of the square.
+    //as the playSound function has the (name) as the param
     playSound(userChosenColor)
     })
 
 function nextSequence(){
     let randomNum = Math.floor(Math.random()*4);
     console.log(randomNum)
+    //the buttonColors is an array and it could be any randomNum, buttonColors[2] or [1]
     let randomChosenColor = buttonColors[randomNum];
     console.log(randomChosenColor)
+    //here the gamePattern is an empty array and we push the randomChosenColor into this empty array
     gamePattern.push(randomChosenColor);
     console.log(gamePattern);
 
@@ -46,6 +51,7 @@ if (randomChosenColor ==="green") {
     }
 
 }
+//since all mp3 files have the name of the colour, hence concat. to make the audio runs!
 function playSound (name) {
     let audioRun = new Audio("sounds/" + name + ".mp3")
     audioRun.play();
